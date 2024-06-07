62b00bad4d3bc2107cbf90a7f800f419c0cfba4c
Andoree <and2900@inbox.ru>
2024-06-07 12:56:20 +0300

    Fix

--- molretrieval/train_pipeline/train_eval_pipeline.py
+++ molretrieval/train_pipeline/train_eval_pipeline.py
@@ -94,6 +94,7 @@ class RegressionDataset(Dataset):
             "attention_mask": self.tokenized_smiles[idx]["attention_mask"][0],
             "labels": self.target_values[idx]}
 
+
 def main(args):
     input_data_dir = args.input_data_dir
     additional_test_sets = args.additional_test_sets
@@ -114,9 +115,9 @@ def main(args):
     input_train_path = os.path.join(input_data_dir, "train.csv")
     input_valid_path = os.path.join(input_data_dir, "valid.csv")
     input_test_path = os.path.join(input_data_dir, "test.csv")
-    train_df = pd.read_csv(input_train_path)#.fillna(0)
-    val_df = pd.read_csv(input_valid_path)#.fillna(0)
-    test_df = pd.read_csv(input_test_path)#.fillna(0)
+    train_df = pd.read_csv(input_train_path)  # .fillna(0)
+    val_df = pd.read_csv(input_valid_path)  # .fillna(0)
+    test_df = pd.read_csv(input_test_path)  # .fillna(0)
 
     logging.info(f"Loaded data: Train - {train_df.shape}, Val - {val_df.shape}, Test - {test_df.shape}")
     config_dict = load_config_dict(config_path=input_config_path)
@@ -134,11 +135,10 @@ def main(args):
     train_df[smiles_col] = train_df[smiles_col].fillna("")
     val_df[smiles_col] = val_df[smiles_col].fillna("")
     test_df[smiles_col] = test_df[smiles_col].fillna("")
-
-    train_df[target_col] = train_df[target_col].fillna(0)
-    val_df[target_col] = val_df[target_col].fillna(0)
-    test_df[target_col] = test_df[target_col].fillna(0)
-
+    if target_col is not None and target_col.strip() != "None":
+        train_df[target_col] = train_df[target_col].fillna(0)
+        val_df[target_col] = val_df[target_col].fillna(0)
+        test_df[target_col] = test_df[target_col].fillna(0)
 
     train_dataset = RegressionDataset(train_df, smiles_col=smiles_col, target_col=target_col, max_length=max_length,
                                       tokenizer=tokenizer, classes_or_smiles_first=classes_or_smiles_first,
